62e21614e9c264c346bcadf95aa107dd4cf86112
jialixin <958887233@qq.com>
2016-09-27 01:49:46 +0800

    improve page

--- works/works/apps/blog/views.py
+++ works/works/apps/blog/views.py
@@ -5,13 +5,17 @@ from works.apps.blog.models import Blog, Category
 # Create your views here.
 
 def home(request):
-    blogs = Blog.objects.all().order_by('-date_created')[:5]
+    blogs = Blog.objects.all().order_by('-date_created')[:3]
+    categorys = Category.objects.all()
 
-    return render(request,'blog/index.html', {'blogs': blogs})
+    return render(request,'blog/index.html', {'blogs': blogs, 'categorys': categorys})
 
 
-def full_width(request):
-    blogs = Blog.objects.all().order_by('-date_created')
+def full_width(request, category=None):
+    if category:
+        blogs = Blog.objects.filter(category__name_id=category).order_by('-date_created')
+    else:
+        blogs = Blog.objects.all().order_by('-date_created')
     return render(request, 'blog/full-width.html', {'blogs': blogs})
 
 def about(request):
