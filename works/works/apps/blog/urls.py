276e4c6f7cd51340e40ff207ec9ddd509c407d07
jialixin <958887233.com>
2018-01-11 23:54:24 +0800

    修改部分页面功能

--- works/works/apps/blog/urls.py
+++ works/works/apps/blog/urls.py
@@ -1,12 +1,11 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('works.apps.blog.views',
-    url(r'^$', 'home', name='home'),
+    url(r'^$', 'index', name='index'),
     url(r'^full-width/$', 'full_width', name='full-width'),
     url(r'^about/$', 'about', name='about'),
     url(r'^contact/$', 'contact', name='contact'),
     url(r'^blog-(?P<id>\d+)/$', 'detail', name='detail'),
     url(r'^blogs/$', 'all_blogs', name='blogs'),
     url(r'^edit/$', 'blog_form', name='blog-form' ),
-    url(r'^wxdemo/$', 'wxdemo', name='wxdemo'),
 )
--- works/works/apps/blog/views.py
+++ works/works/apps/blog/views.py
@@ -6,25 +6,36 @@ from works.apps.blog.forms import BlogForm
 from django.http import JsonResponse, HttpResponse
 
 
-# Create your views here.
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    for i in range(0, len(l), n):
+        yield l[i:i+n]
 
-def home(request):
+
+def index(request):
     blogs = Blog.objects.all().order_by('-date_created')[:4]
     categorys = Category.objects.all()
-    blogs = [blogs[:2], blogs[2:]]
+    display_blogs = chunks(blogs, 2)
 
-    return render(request,'blog/index.html', {'blogs': blogs, 'categorys': categorys})
+    return render(
+        request,
+        'blog/index.html',
+        {'blogs': blogs, 'display_blogs': display_blogs, 'categorys': categorys}
+    )
 
 
 def full_width(request, category=None):
     if category:
-        blogs = Blog.objects.filter(category__name_id=category).order_by('-date_created')
+        blogs = Blog.objects.filter(category__name_id=category) \
+                .order_by('-date_created')
     else:
         blogs = Blog.objects.all().order_by('-date_created')
     return render(request, 'blog/full-width.html', {'blogs': blogs})
 
+
 def about(request):
     return render(request, 'blog/about.html',{})
+
 
 def contact(request):
     return render(request, 'blog/contact.html', {})
@@ -34,13 +45,16 @@ def detail(request, id):
     blog = get_object_or_404(Blog, id=id)
     return render(request, 'blog/detail.html', {'blog': blog})
 
+
 def all_blogs(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/blogs.html', {'blogs': blogs})
 
+
 def category(request):
     blogs = Blog.objects.all()
     return render(request, 'blog/category.html', {'blogs': blogs})
+
 
 def blog_form(request):
     categorys = Category.objects.all()
@@ -49,9 +63,8 @@ def blog_form(request):
     if request.method == 'POST':
         blog_form = BlogForm(request.POST)
 
-    return render(request, 'blog/edit.html', {'form': blog_form, 'categorys': categorys})
-
-from django.core import serializers
-def wxdemo(request):
-    data = serializers.serialize('json', Blog.objects.all())
-    return HttpResponse(data, content_type="application/json")
+    return render(
+        request,
+        'blog/edit.html',
+        {'form': blog_form, 'categorys': categorys}
+    )
